9c0dca00842d06dd1273ece5d9c5e1d1948779f8
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-05-02 01:00:38 +0200

    test de la creation des tables de la base de donnees

---
+++ pglp_5_2/src/test/java/Abdelhafid/pglp_5_2/Jdbc/TestInitializer.java
@@ -0,0 +1,24 @@
+package Abdelhafid.pglp_5_2.Jdbc;
+
+import org.junit.Test;
+
+public class TestInitializer {
+
+	
+	
+	@Test
+	public void CreateTables() throws TableExist {
+	Initializer base_donnes= new Initializer();
+	 base_donnes.CreateAllTables();
+	 
+	}
+	
+	
+	/*
+	@Test
+	public void dropCreateTables () throws TableExist, SQLException {
+		Initializer base_donnes= new Initializer();
+		 base_donnes.dropCreateTables();
+	}
+	 */
+}
